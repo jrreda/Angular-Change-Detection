b1a3260193dac9e9cf2f49730e33c18338ccd35d
Reda <mahmoudreda457@gmail.com>
2026-05-31 18:52:53 +0300

    remove signals

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -2,10 +2,13 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class MessagesService {
-  private messages = signal<string[]>([]);
-  allMessages = this.messages.asReadonly();
+  private messages: string[] = [];
+
+  get allMessages() {
+    return [...this.messages];
+  }
 
   addMessage(message: string) {
-    this.messages.update((prevMsg) => [...prevMsg, message]);
+    this.messages = [...this.messages, message];
   }
 }
